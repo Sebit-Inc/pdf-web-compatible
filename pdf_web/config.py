7f14accf904f6599fabbc07649e7aac8c7e675cc
tunahan_bircan <tunahan.bircan@sebit.com.tr>
2026-09-25 10:07:00 +0300

    Release 1.0.1 so existing exes can pick up an in-app update.

--- pdf_web/config.py
+++ pdf_web/config.py
@@ -20,7 +20,7 @@ COLORS = {
     "border":       "#2a3148",
 }
 
-APP_VERSION = "1.0.0"
+APP_VERSION = "1.0.1"
 UPDATE_FEED_URL = (
     "https://github.com/Sebit-Inc/pdf-web-compatible/"
     "releases/latest/download/latest.json"
